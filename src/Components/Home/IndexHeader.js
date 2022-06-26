659b9b0add531b2acfec82581543b69f47c824aa
noefortyau <leiva.noe.86@gmail.com>
2022-06-26 11:29:30 -0400

    removed s

--- src/Components/Home/IndexHeader.js
+++ src/Components/Home/IndexHeader.js
@@ -14,7 +14,7 @@ function Header () {
                     </div>
 
                     <div className="col-sm-12 col-md-6 col-lg-6 align-self-center">
-                        <h1 className="display-4">ABOUT USs</h1>
+                        <h1 className="display-4">ABOUT US</h1>
                         <p className="lead">Go and Build it’s an organization committed to improve health and safety living conditions for families in extreme poverty, through the construction of sustainable homes; providing a secure and dignified place to live.</p>
                     </div>
                     
