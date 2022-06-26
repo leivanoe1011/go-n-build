1f6a79d13d92180c666e810e763b883846b2c34e
noefortyau <leiva.noe.86@gmail.com>
2022-06-26 11:41:57 -0400

    aligned header

--- src/Components/Home/IndexHeader.js
+++ src/Components/Home/IndexHeader.js
@@ -7,13 +7,13 @@ function Header () {
 
     return (
         <>
-                <div className="row justify-content-lg-center wow animate__animated animate__fadeInLeft animate__slower">
+                <div className="row justify-content-md-center justify-content-lg-center wow animate__animated animate__fadeInLeft animate__slower">
                 
-                    <div className="col-sm-12 col-md-6 col-lg-6 align-self-center">
+                    <div className="col-sm-12 col-md-5 col-lg-5 align-self-center text-center">
                         <img src={poorKidImage}/>
                     </div>
 
-                    <div className="col-sm-12 col-md-6 col-lg-6 align-self-center">
+                    <div className="col-sm-12 col-md-5 col-lg-5 align-self-center text-left">
                         <h1 className="display-4">ABOUT US</h1>
                         <p className="lead">Go and Build it’s an organization committed to improve health and safety living conditions for families in extreme poverty, through the construction of sustainable homes; providing a secure and dignified place to live.</p>
                     </div>
