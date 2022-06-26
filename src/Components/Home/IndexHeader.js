4b8893e09539a180d750990861869d8289ac3c1c
noefortyau <leiva.noe.86@gmail.com>
2022-06-26 12:40:20 -0400

    updated header size

--- src/Components/Home/IndexHeader.js
+++ src/Components/Home/IndexHeader.js
@@ -16,7 +16,7 @@ function Header () {
                     <div className="col-sm-12 col-md-6 col-lg-6 align-self-center text-left">
                         <h1 className="display-4">ABOUT US</h1>
                         <div className="row">
-                           <p className="lead col-sm-12 col-md-6 col-lg-6">
+                           <p className="lead col-sm-12 col-md-8 col-lg-8">
                             Go and Build it’s an organization committed to improve health and 
                             safety living conditions for families in extreme poverty, through the construction of sustainable homes; 
                             providing a secure and dignified place to live.
